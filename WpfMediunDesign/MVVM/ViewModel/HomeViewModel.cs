a68fe813c248c946903e731fed86b35c6a58a5ac
Puneet Kushwah <hacker7xy@gmail.com>
2021-08-03 22:41:50 +0530

    Add project files.

---
+++ WpfMediunDesign/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMediunDesign.Core;
+
+namespace WpfMediunDesign.MVVM.ViewModel
+{
+    class HomeViewModel
+    {
+        
+    }
+}
